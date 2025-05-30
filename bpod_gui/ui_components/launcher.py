6210f40615e53c663a06977b67faec53394dc6f5
austin2118ace <austin2118ace@gmail.com>
2025-05-29 21:46:40 -0400

    Basic layout of launcher

--- bpod_gui/ui_components/launcher.py
+++ bpod_gui/ui_components/launcher.py
@@ -1,9 +1,42 @@
-from PySide6.QtWidgets import QMainWindow
+import sys
+
+from PySide6.QtWidgets import QMainWindow, QGridLayout, QPushButton, QWidget, QSizePolicy, QMenuBar
 from PySide6.QtCore import QSize
 
 class Launcher(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Bpod Launcher")
-        self.setMinimumSize(QSize(400, 200))
+        self.setFixedSize(QSize(400, 200))
 
+        self.main_layout = QGridLayout()
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(self.main_layout)
+        self.protocol_button = QPushButton()
+        self.manual_control_button = QPushButton()
+        self.configure_button = QPushButton()
+        self.protocol_button.setText('Launch Protocol!')
+        self.manual_control_button.setText('Manual Control')
+        self.configure_button.setText('Configuration')
+        self.protocol_button.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)
+        self.manual_control_button.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Preferred)
+        self.configure_button.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Preferred)
+        self.manual_control_button.setMinimumSize(QSize(60, 60))
+        self.configure_button.setMinimumSize(QSize(60, 60))
+        self.main_layout.addWidget(self.protocol_button, 0, 0, 1, -1)
+        # Top Left grid and span the first row and both columns
+        self.main_layout.addWidget(self.manual_control_button, 1, 0)
+        self.main_layout.addWidget(self.configure_button, 1, 1)
+
+        menubar = QMenuBar()
+        file_menu = menubar.addMenu(u'&File')
+        exit_action = file_menu.addAction(u'&Exit')
+        exit_action.triggered.connect(sys.exit)  # We may have cleanup to do here later
+
+        about_menu = menubar.addMenu(u'&About')
+        about_menu.triggered.connect(Launcher.__dummy__)  # Launch about dialog
+        self.setMenuBar(menubar)
+
+    def __dummy__(self):
+        pass
