539a20f19a3a573d9620c85543f07dd81241f304
austin2118ace <austin2118ace@gmail.com>
2025-06-10 19:12:26 -0400

    refactor layout; add manual_control module

--- bpod_gui/start.py
+++ bpod_gui/start.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide6 import QtWidgets
-from .base_ui.launcher import Launcher
+from .ui.launcher import Launcher
 
 def launch_gui():
     app = QtWidgets.QApplication(sys.argv)
---
+++ bpod_gui/ui/manual_control.py
@@ -0,0 +1,7 @@
+from PySide6.QtWidgets import QDockWidget, QWidget, QMainWindow
+
+
+class manual_control(QDockWidget):
+
+    def __init__(self):
+        super().__init__()
