74f97010e0049360424b75082763b7c5acfa2b05
austin2118ace <austin2118ace@gmail.com>
2025-10-07 02:19:13 -0400

    Create/show calibration UI

--- bpod_gui/start.py
+++ bpod_gui/start.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from qtpy import QtWidgets
 from bpod_gui.ui.console import ManualControl
 
+from bpod_gui.ui.calibration import calibration_ui
 logging.basicConfig()
 main_logger = logging.getLogger()
 main_logger.setLevel(logging.NOTSET)
@@ -13,12 +14,14 @@ def launch_gui():
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle("Fusion")
     mc = ManualControl(main_logger)
+    calibration_widget = calibration_ui.CalibrationUI()
 
     stylesheets = get_stylesheets(Path("bpod_gui/qss"))
     apply_stylesheet(app, stylesheets, "base")
     apply_stylesheet(mc, stylesheets, "manual_control_interface")
 
     # mc.show()
+    calibration_widget.show()
     app.exec()
 
 def get_stylesheets(style_sheet_dir: Path):
